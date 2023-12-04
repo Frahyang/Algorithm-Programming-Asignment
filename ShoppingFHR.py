8aefca5704a1455054558a1c13a7471edce7f9d3
Frahyang <142311241+Frahyang@users.noreply.github.com>
2023-12-04 16:11:46 +0700

    Update ShoppingFHR.py

--- ShoppingFHR.py
+++ ShoppingFHR.py
@@ -3,36 +3,39 @@ from MenuFHR import Menu
 #Function to create a list of objects
 def createListFHR():
     listOfItems = []
-    numberOfItems = input("How many items will you order today? ")
-    if numberOfItems > 0:
-        for itemNumber in numberOfItems:
-            print("Item #" + itemNumber)
-            food = input("Enter food: ")
-            foodInPounds = input("Enter amount of pounds: ")
-            listOfItems.append(food, foodInPounds)
-    else:
-        print("Number of items must be at least 1")
+    while True:
+        numberOfItems = int(input("How many items will you order today? "))
+        if numberOfItems > 0:
+            for itemNumber in range(numberOfItems):
+                print("Item #" + str(itemNumber + 1))
+                food = input("Enter food: ")
+                foodAmount = float(input("Enter amount of pounds: "))
+                object = Menu(food, foodAmount)
+                listOfItems.append(object)
+        else:
+            print("Number of items must be at least 1")
+            continue
+        return listOfItems
 
 #Function to display the contents of the list
 def displayListFHR(listOfFood):
     for food in listOfFood:
         print("Item: " + food.foodName)
-        print("Amount: " + food.foodAmount)
-        print("Price per pound: " + food.standardPrice)
-        print("Price of order: " + food.totalPrice)
+        print("Amount: " + str(food.foodAmount))
+        print("Price per pound: " + str(food.standardPrice))
+        print("Price of order: " + str(food.totalPrice))
 
 #Function to calculate the total cost of all items
-def totalCostFHR(listOfFood):
-    foodPrice = ()
+def totalCostFHR(foodPrice):
+    listOfFood = []
     for price in foodPrice:
-        price.append(price.foodPrices)
-    sum(foodPrice)
+        listOfFood.append(price.totalPrice)
+    return sum(listOfFood)
 
 #The main function
-def main():
-    createListFHR()
-    displayListFHR()
-    totalCostFHR()
+def groceriesFHR():
+    listOfFood = createListFHR()
+    displayListFHR(listOfFood)
+    print("Total cost: " + str(totalCostFHR(listOfFood)))
 
-main()
-
+groceriesFHR()
