54b87910aad0a7a16d9561a6171bccf7b99228db
Frahyang <142311241+Frahyang@users.noreply.github.com>
2023-12-04 16:11:13 +0700

    Update MenuFHR.py

--- MenuFHR.py
+++ MenuFHR.py
@@ -1,37 +1,32 @@
 class Menu:
 #The initialiser
     def __init__(self, foodName, foodAmount):
-        self.__foodName = foodName
-        self.__foodAmount = foodAmount
-        self.__standardPrice = self.__PriceList()
+        self.foodName = foodName
+        self.foodAmount = foodAmount
+        self.__PriceList()
         self.totalPrice = self.calculatePriceFHR()
 
 #The private method to store the list of items
     def __PriceList(self):
-        if self.__foodName == "Dry Cured Iberian Ham":
-            self.__standardPrice = 177.80
-        elif self.__foodName == "Wagyu Steak":
-            self.__standardPrice = 450.00
-        elif self.__foodName == "Matsutake Mushrooms":
-            self.__standardPrice = 272.00
-        elif self.__foodName == "Kopi Luwak Coffe":
-            self.__standardPrice = 306.50
-        elif self.__foodName == "Moose Cheese":
-            self.__standardPrice = 487.20
-        elif self.__foodName == "White Truffles":
-            self.__standardPrice = 3600.00
-        elif self.__foodName == "Blue Fin Tuna":
-            self.__standardPrice = 3603.00
-        elif self.__foodName == "Le Bonnotte Potatoes":
-            self.__standardPrice = 270.81
+        if self.foodName == "Dry Cured Iberian Ham":
+            self.standardPrice = 177.80
+        elif self.foodName == "Wagyu Steaks":
+            self.standardPrice = 450.00
+        elif self.foodName == "Matsutake Mushrooms":
+            self.standardPrice = 272.00
+        elif self.foodName == "Kopi Luwak Coffe":
+            self.standardPrice = 306.50
+        elif self.foodName == "Moose Cheese":
+            self.standardPrice = 487.20
+        elif self.foodName == "White Truffles":
+            self.standardPrice = 3600.00
+        elif self.foodName == "Blue Fin Tuna":
+            self.standardPrice = 3603.00
+        elif self.foodName == "Le Bonnotte Potatoes":
+            self.standardPrice = 270.81
         else:
-            self.__standardPrice = 0.00
+            self.standardPrice = 0.00
 
 #Public method to calculate the price
     def calculatePriceFHR(self):
-        return self.__foodAmount * self.__standardPrice
-    
-    def __str__(self):
-        print(self.totalPrice)
-
-Menu("Dry Cured Iberian Ham", 1)
+        return self.foodAmount * self.standardPrice
